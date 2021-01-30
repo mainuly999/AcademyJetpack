2f399ffe398e87760a6f7f157b238aaffaabb59d
mainuly999 <mainuly999@gmail.com>
2021-01-30 22:38:07 +0800

    persiapan 1.0

---
+++ app/src/main/java/com/ainul/academy/detail/DetailCourseActivity.kt
@@ -0,0 +1,20 @@
+package com.ainul.academy.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ainul.academy.R
+
+class DetailCourseActivity : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_COURSE = "extra_course"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_course)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
--- app/src/main/java/com/ainul/academy/home/HomeActivity.kt
+++ app/src/main/java/com/ainul/academy/home/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.ainul.academy
+package com.ainul.academy.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ainul.academy.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
