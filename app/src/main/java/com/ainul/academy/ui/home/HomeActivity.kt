27641cb1efc84742812d0478aabd697f35a7cc23
mainuly999 <mainuly999@gmail.com>
2021-02-02 15:16:29 +0800

    Add SectionPager in Home Activity

--- app/src/main/java/com/ainul/academy/ui/home/HomeActivity.kt
+++ app/src/main/java/com/ainul/academy/ui/home/HomeActivity.kt
@@ -3,10 +3,20 @@ package com.ainul.academy.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ainul.academy.R
+import com.ainul.academy.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionPagerAdapter(this, supportFragmentManager)
+        binding.viewPager.adapter = sectionsPagerAdapter
+        binding.tabs.setupWithViewPager(binding.viewPager)
+
+        supportActionBar?.elevation = 0f
     }
 }
